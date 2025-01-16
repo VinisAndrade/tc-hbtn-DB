b3d8a696f79a030d8c4e7079eefd503ca8694f67
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-15 22:09:27 -0300

    models

--- jpa_hibernate/src/main/java/models/ProdutoModel.java
+++ jpa_hibernate/src/main/java/models/ProdutoModel.java
@@ -1,16 +1,7 @@
-package models;
-
-import entities.Produto;
-
-import javax.persistence.*;
-import java.util.List;
-
 public class ProdutoModel {
-
     public void create(Produto p) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("admin-jpa");
         EntityManager em = emf.createEntityManager();
-
         try {
             em.getTransaction().begin();
             em.persist(p);
@@ -21,23 +12,19 @@ public class ProdutoModel {
             em.close();
         }
     }
-
     public Produto findById(Long id) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("admin-jpa");
         EntityManager em = emf.createEntityManager();
         return em.find(Produto.class, id);
     }
-
     public List<Produto> findAll() {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("admin-jpa");
         EntityManager em = emf.createEntityManager();
         return em.createQuery("from Produto", Produto.class).getResultList();
     }
-
     public void update(Produto p) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("admin-jpa");
         EntityManager em = emf.createEntityManager();
-
         try {
             em.getTransaction().begin();
             em.merge(p);
@@ -46,11 +33,9 @@ public class ProdutoModel {
             em.close();
         }
     }
-
     public void delete(Long id) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("admin-jpa");
         EntityManager em = emf.createEntityManager();
-
         try {
             em.getTransaction().begin();
             Produto p = em.find(Produto.class, id);

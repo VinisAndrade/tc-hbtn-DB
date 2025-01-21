feff7d439d610ae10b0bbe1a8ab9ae9ebcfee731
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-21 14:53:36 -0300

    correção

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
@@ -1,18 +1,61 @@
-package com.techcamps.gestao.cursos.entities;
+package entities;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.List;
 
 @Entity
 public class Professor {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nomeCompleto;
     private String matricula;
+    private String email;
 
     @OneToMany(mappedBy = "professor")
     private List<Curso> cursos;
 
-    // Getters e Set
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Curso> getCursos() {
+		return cursos;
+	}
+
+	public void setCursos(List<Curso> cursos) {
+		this.cursos = cursos;
+	}
+
+    
+}
